8bff4573237409ca1160c9464a095d4684e8da5a
Carieska Berliana Novita Kusuma Wardani <riskaberliana123@gmail.com>
2024-04-14 22:23:13 +0700

    pw8 prak 4

--- pertemuan8/upload.js
+++ pertemuan8/upload.js
@@ -1,19 +1,49 @@
-$(document).ready(function(){
-    $('#upload-form').submit(function(event){
-        event.preventDefault(); 
+// $(document).ready(function(){
+//     $('#upload-form').submit(function(event){
+//         event.preventDefault(); 
+//         var formData = new FormData(this);
+
+//         $.ajax({
+//             type: 'POST',
+//             url: 'upload_ajax.php',
+//             data: formData,
+//             cache: false,
+//             contentType: false,
+//             processData: false,
+//             success: function(response){
+//                 $('#status').html(response);
+//             },
+//             error: function(){
+//                 $('#status').html('Terjadi kesalahan saat mengunggah file.');
+//             }
+//         });
+//     });
+// });
+
+$(document).ready(function() {
+    $('#file').change(function() {
+        if(this.files.length > 0) {
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
+    $('#upload-form').submit(function(e) {
+        e.preventDefault();
+
         var formData = new FormData(this);
 
         $.ajax({
-            type: 'POST',
-            url: 'upload_ajax.php',
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            success: function(response){
+            type : 'POST',
+            url : 'upload_ajax.php',
+            data : formData,
+            cache : false,
+            contentType : false,
+            processData : false,
+            success : function(response) {
                 $('#status').html(response);
             },
-            error: function(){
+            error : function() {
                 $('#status').html('Terjadi kesalahan saat mengunggah file.');
             }
         });
